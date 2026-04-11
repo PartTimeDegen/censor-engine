7ef7257319597e2b0ea1257b590e1f4f2ec722f8
ptd <>
2026-04-11 01:08:02 +0100

    moved files; deleted old file

--- src/censor_engine/models/lib_models/styles/sub_variants.py
+++ src/censor_engine/models/lib_models/styles/sub_variants.py
@@ -1,5 +1,8 @@
 import cv2
 import numpy as np
+from matplotlib import font_manager
+from PIL import Image as PImage
+from PIL import ImageDraw, ImageFont
 
 from censor_engine.models.enums import StyleType
 from censor_engine.models.structs.colours import Colour
@@ -120,42 +123,145 @@ class StyliseStyle(Style):
 class TextStyle(Style):
     style_type: StyleType = StyleType.TEXT
 
-    def put_text(
+    # Structural Helpers
+    def get_font(self, font: str) -> str:
+        fonts = font_manager.findSystemFonts(fontpaths=None, fontext="ttf")
+        font_path = None
+        for font_name in fonts:
+            if font in font_name:
+                font_path = font_name
+                break
+        if font_path is None:
+            msg = f"Trying to use unavailable font! {fonts}"
+            raise ValueError(msg)
+
+        return font
+
+    # Basic Vector Helpers
+    def _add_cords(
+        self,
+        a: tuple[int, int],
+        b: tuple[int, int],
+    ) -> tuple[int, int]:
+        return (a[0] + b[0], a[1] + b[1])
+
+    def _subtract_cords(
+        self,
+        a: tuple[int, int],
+        b: tuple[int, int],
+    ) -> tuple[int, int]:
+        return (a[0] - b[0], a[1] - b[1])
+
+    # Aids to Text Randomly being Bottom Left rather than Top Left
+    def _normalise_text_coord(
+        self,
+        coords: tuple[int, int],
+        size: tuple[int, int],
+    ) -> tuple[int, int]:
+        return (coords[0], coords[1] - size[1])
+
+    def _get_centre(self, size: tuple[int, int]) -> tuple[int, int]:
+        return (int(size[0] * 0.5), int(size[1] * 0.5))
+
+    def _convert_rel_box_to_coords_and_size(
+        self, rel_box: tuple[int, int, int, int]
+    ) -> tuple[tuple[int, int], tuple[int, int]]:
+        coords = rel_box[:2]
+        size = rel_box[2:]
+        return coords, size
+
+    def _get_middle_coords(
+        self,
+        rel_box: tuple[int, int, int, int],
+    ) -> tuple[int, int]:
+        coords, size = self._convert_rel_box_to_coords_and_size(rel_box)
+        half_size = self._get_centre(size)
+        return (
+            coords[0] + half_size[0],
+            coords[1] + half_size[1],
+        )
+
+    def _convert_middle_to_bottom_left_coords(
+        self,
+        coords: tuple[int, int],
+        size: tuple[int, int],
+    ) -> tuple[int, int]:
+        middle = self._get_centre(size)
+        return (
+            coords[0] - middle[0],
+            coords[1] + middle[1],
+        )
+
+    # Pillow Module for Custom Fonts
+    def _put_custom_font(
         self,
         image: Image,
-        text: str | list[str],
-        coord_origin: tuple[int, int],
-        color: Colour,
-        font: int = cv2.FONT_HERSHEY_SIMPLEX,
-        font_scale: int = 1,
-        thickness: int = 2,
-        line_type: int = cv2.LINE_AA,
-        line_spacing: float = 1.2,
-    ) -> Image:
-        if isinstance(text, list):
-            text = "\n".join(text)
+        word: str,
+        coords: tuple[int, int],
+        font: str,
+        font_percent: float,
+        colour: tuple[int, int, int],
+        mask_size: tuple[int, int],
+        outline_width: int,
+        outline_colour: tuple[int, int, int],
+    ):
+        # Convert to Pillow
+        img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        img_pil = PImage.fromarray(img)
 
-        coord_x, coord_y = coord_origin
-        for index, line in enumerate(text.split("\n")):
-            y_line = int(
-                coord_y
-                + index
-                * (
-                    cv2.getTextSize(line, font, font_scale, thickness)[0][1]
-                    * line_spacing
-                ),
+        # Create Draw Object
+        draw = ImageDraw.Draw(img_pil)
+
+        # Test Word on Base Size
+        base_font_loaded = ImageFont.truetype(font + ".ttf", 20)
+        base_bbox = draw.textbbox(
+            (0, 0),
+            word,
+            font=base_font_loaded,
+            font_size=20,
+        )
+        base_sizes = (
+            base_bbox[2] - base_bbox[0],
+            base_bbox[3] - base_bbox[1],
+        )
+
+        # Calculate New Font Size
+        font_size = int(
+            font_percent
+            * 20
+            * min(
+                (
+                    mask_size[0] / base_sizes[0],
+                    mask_size[1] / base_sizes[1],
+                )
             )
-            cv2.putText(
-                image,
-                line,
-                (coord_x, y_line),
-                font,
-                font_scale,
-                color.value,
-                thickness,
-                line_type,
-            )
-        return image
+        )
+        font_loaded = ImageFont.truetype(font + ".ttf", font_size)
+        bbox = draw.textbbox(
+            (0, 0),
+            word,
+            font=font_loaded,
+            font_size=font_size,
+        )
+
+        # Align to Middle
+        text_w = bbox[2] - bbox[0]
+        text_h = bbox[3] - bbox[1]
+        top_left_coords = (
+            int(coords[0] - text_w * 0.5 - bbox[0]),
+            int(coords[1] - text_h * 0.5 - bbox[1]),
+        )
+
+        # Draw Text
+        draw.text(
+            top_left_coords,
+            word,
+            font=font_loaded,
+            fill=colour,
+            stroke_width=outline_width,
+            stroke_fill=outline_colour,
+        )
+        return cv2.cvtColor(np.array(img_pil), cv2.COLOR_RGB2BGR)
 
 
 class DevStyle(TextStyle):
